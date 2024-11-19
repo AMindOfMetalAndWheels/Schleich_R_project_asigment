b83e09caf56e1af7d549a53a6b87cca1d8abd501
Fabian Schleich <fabianschleich@gmx.net>
2024-11-19 21:39:55 +0100

    New file generted for asignment

--- Day1_Assignment1/code/Day1_asignment1_code_new.R
+++ Day1_Assignment1/code/Day1_asignment1_code_new.R
@@ -12,3 +12,39 @@ library(tidyverse)
 library(renv)
 #make snapshot
 renv::snapshot()
+# load data 
+data(iris)
+#explore structure
+head(iris)
+summary(iris)
+str(iris)
+
+
+#data manipulation:
+
+#summary of the dataset
+summary <- summary(iris) 
+
+#summerises numeric columns (not the species)
+iris %>% summarize_if(is.numeric, mean) 
+
+#filterers data with drop_na function then removes duplicated rows
+clean.data <- iris %>% drop_na() %>% unique() 
+
+#summary of filtered data
+summary(clean.data)
+
+#mean per species calculated
+iris %>% group_by(Species) %>% summarize_all(mean)
+
+#random sampling
+sample(c("A", "B", "C"), size = 10, replace = TRUE)
+
+
+#Visualization: 
+library(GGally)
+ggpairs(iris, aes(color = Species))
+
+#save liberary state
+
+renv::snapshot()
