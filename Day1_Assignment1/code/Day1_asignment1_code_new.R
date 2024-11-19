69009ea00fbb7d608a152731801c339d2dd6e38e
Fabian Schleich <fabianschleich@gmx.net>
2024-11-19 20:52:47 +0100

    Day1 assingment1

---
+++ Day1_Assignment1/code/Day1_asignment1_code_new.R
@@ -0,0 +1,14 @@
+#########
+#19/11/24
+#Fabian Schleich
+#Asingment 1:
+#
+#Install tidyverse
+#then
+#install.packages("tidyverse")
+#load
+library(tidyverse)
+#load
+library(renv)
+#make snapshot
+renv::snapshot()
